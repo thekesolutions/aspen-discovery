adea1d17cbf87ee08336d5c9de32cf61bfdeeca0
Jacob O'Mara <jacobomara901@gmail.com>
2023-06-05 16:22:11 +0100

    More permissions fixes

--- dockerrun.sh
+++ dockerrun.sh
@@ -12,7 +12,7 @@ chmod -R a+wr  /usr/local/aspen-discovery/tmp/
 
 chown -R aspen /data/aspen-discovery/test.localhostaspen/solr7;
 
-chown -R /usr/local/aspen-discovery/sites/default/solr-7.6.0/server/;
+chown -R a+wr /usr/local/aspen-discovery/sites/default/solr-7.6.0/server/
 
 service apache2 start;
 
