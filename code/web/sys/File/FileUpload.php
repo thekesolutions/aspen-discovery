9306ed30dd64ae88e9c8bea367b41af046a7d940
Lucas Montoya <lucasmontoya13@mi.unc.edu.ar>
2024-03-18 09:37:56 -0300

    A new type of upload "db_file" has been added - db_file will replace the actual type "file" ones it is fully tested. - All file uploaded as db_file will be store on database - Thumbnail related to a db_file changes its path in the file system - A new method to get a thumbnail has been implemented

--- code/web/sys/File/FileUpload.php
+++ code/web/sys/File/FileUpload.php
@@ -5,7 +5,9 @@ class FileUpload extends DataObject {
 	public $id;
 	public $title;
 	public $fullPath;
+	public $uploadedFileData;
 	public $thumbFullPath;
+	public $thumbnailData;
 	public $type;
 
 	public function getUniquenessFields(): array {
@@ -13,6 +15,7 @@ class FileUpload extends DataObject {
 	}
 
 	static function getObjectStructure($context = ''): array {
+		global $serverName;
 		return [
 			'id' => [
 				'property' => 'id',
@@ -37,7 +40,7 @@ class FileUpload extends DataObject {
 			],
 			'fullPath' => [
 				'property' => 'fullPath',
-				'type' => 'file',
+				'type' => 'db_file',
 				'label' => 'Full Path',
 				'description' => 'The path of the file on the server',
 			],
@@ -46,6 +49,7 @@ class FileUpload extends DataObject {
 				'type' => 'text',
 				'label' => 'Thumbnail Full Path',
 				'description' => 'The path of the generated thumbnail on the server',
+				'dirPath' => '/data/aspen-discovery/' . $serverName . '/uploads/web_builder_pdf/thumbnail',
 				'readOnly' => true,
 			],
 		];
@@ -56,7 +60,7 @@ class FileUpload extends DataObject {
 	}
 
 	function insert($context = '') {
-		$this->makeThumbnail();
+		$this->genPdfThumbnail();
 		return parent::insert();
 	}
 
@@ -68,9 +72,11 @@ class FileUpload extends DataObject {
 		return parent::update();
 	}
 
+
+
 	/** @noinspection PhpUnused */
 	function makeThumbnail() {
-		if ($this->type == 'web_builder_pdf' && !empty($this->fullPath)) {
+		if ($this->type == 'web_builder_pdf') {
 			$destFullPath = $this->fullPath;
 			$thumbFullPath = '';
 			if (extension_loaded('imagick')) {
@@ -95,6 +101,78 @@ class FileUpload extends DataObject {
 				}
 			}
 		}
+	}
+
+
+
+	function genPdfThumbnail() {
+		if ($this->type == 'web_builder_pdf'){
+			$isWrote = 0;
+			if (isset($this->uploadedFileData)){
+				//Create tmpfile where will be store the blob temporarily
+				$tmpDir = $this->tempdir();
+				$tmpFullPath = $tmpDir . '/' . $this->getFormatTitle($this->title);
+				//Store the blob into the tmpfile
+				$isWrote = file_put_contents($tmpFullPath,$this->uploadedFileData);
+			}
+			if (extension_loaded('imagick') && $isWrote > 0) {
+				$dirPath = self::getObjectStructure()['thumbFullPath']['dirPath'];
+				if (!file_exists($dirPath)) {
+					mkdir($dirPath, 0775, true);
+					chgrp($dirPath, 'aspen_apache');
+					chmod($dirPath, 0775);
+				}
+				if (substr($dirPath, -1) == '/') {
+					$dirPath = substr($dirPath, 0, -1);
+				}
+				$target = $dirPath . '/' . $this->getFormatTitle($this->title);
+				$im = new Imagick($tmpFullPath."[0]"); // 0-first page, 1-second page
+				$im->setImageColorspace(255); // prevent image colors from inverting
+				$im->setimageformat("jpg");
+				$im->thumbnailimage(150, 150); // width and height
+				$wroteOk = $im->writeimage($target);
+				$im->clear();
+				$im->destroy();
+				if ($wroteOk) {
+					$thumbFullPath = $target . '.jpg';
+					$this->thumbFullPath = $thumbFullPath;
+				}
+			}
+		}
+	}
+
+	private function tempdir($dir = null, $prefix = 'tmp_', $mode = 0700, $maxAttempts = 1000) {
+		if (is_null($dir)) {
+			$dir = sys_get_temp_dir();
+		}
+		$dir = rtrim($dir, DIRECTORY_SEPARATOR);
+		if (!is_dir($dir) || !is_writable($dir)) {
+			return false;
+		}
+		if (strpbrk($prefix, '\\/:*?"<>|') !== false) {
+			return false;
+		}
+		/* Attempt to create a random directory until it works. Abort if we reach
+		 * $maxAttempts. Something screwy could be happening with the filesystem
+		 * and our loop could otherwise become endless.
+		 */
+		$attempts = 0;
+		do {
+			$path = sprintf('%s%s%s%s', $dir, DIRECTORY_SEPARATOR, $prefix, mt_rand(100000, mt_getrandmax()));
+		} while (
+			!mkdir($path, $mode) &&
+			$attempts++ < $maxAttempts
+		);
+		return $path;
+	}
+
+	private function getFormatTitle($title) {
+		$title = strtolower($title);
+		$title = str_replace(" ", "", $title);
+		$title = preg_replace("/[^a-z0-9]/", "", $title);
+		$extension = pathinfo($title, PATHINFO_EXTENSION);
+
+		return $title;
 	}
 
 	public function okToExport(array $selectedFilters): bool {
