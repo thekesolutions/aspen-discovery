a84107cfd7c59502989a8689ecc2a512db74f02d
Jacob O'Mara <jacob.omara@ptfs-europe.com>
2024-06-04 12:12:28 +0100

    chore: tidy up dockerrun.sh script

--- dockerrun.sh
+++ dockerrun.sh
@@ -4,23 +4,23 @@
 # This script is used to run the docker container
 # cp -R /test.localhostaspen /usr/local/aspen-discovery/sites
 
-service cron start;
+service cron start
 
 mkdir -p /data/aspen-discovery/test.localhostaspen/covers/{small,large,medium,original}
 
 mkdir -p /usr/local/aspen-discovery/tmp/smarty/compile/
 
-mkdir -p /var/log/aspen-discovery/test.localhostaspen 
+mkdir -p /var/log/aspen-discovery/test.localhostaspen
 
 chmod -R a+wr /var/log/
 
-chmod -R a+wr  /usr/local/aspen-discovery/
+chmod -R a+wr /usr/local/aspen-discovery/
 
 chmod -R a+wr /data/aspen-discovery/test.localhostaspen/
 
-chown -R aspen /data/aspen-discovery/test.localhostaspen/solr7;
+chown -R aspen /data/aspen-discovery/test.localhostaspen/solr7
 
-service apache2 start;
+service apache2 start
 
 curl -k http://localhost/API/SystemAPI?method=runPendingDatabaseUpdates
 
