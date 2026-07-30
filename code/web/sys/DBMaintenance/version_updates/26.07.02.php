102d91d2af2ca032af9403f3cd3d0c24e618add2
Mark Noble <mdnoble73@gmail.com>
2026-07-30 10:24:35 -0600

    DIS-2250 OAuth SSO fixes
    - increase username column length to accommodate using email as username
    - properly load account profile based on SSO Settings linked
    - When getting access token send information in post body rather than query string
    - When loading resource owner information send access token in headers rather than as url parameter
    - Update to find users based on username rather than non-existent user_id column
    - Update searchArray to properly handle nesting of array
    - Properly link users to the correct account profile
    - Fix adding administrators based on username field for add administrator
    
    (cherry picked from commit e625440a964881ed087b136ba6e5d99a29b4061b)

---
+++ code/web/sys/DBMaintenance/version_updates/26.07.02.php
@@ -0,0 +1,53 @@
+<?php
+/** @noinspection SqlDialectInspection */
+
+/** @noinspection PhpUnused */
+function getUpdates26_07_02(): array {
+	$now = time();
+
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark n
+		'increase_user_username_column' => [
+			'title' => 'Increase username column in user table',
+			'description' => 'Increase username column in user table',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE user CHANGE COLUMN username username VARCHAR(255) NOT NULL',
+			]
+		], //increase_user_username_column
+
+		//kirstien
+
+		//kodi
+
+		//yanjun
+
+		//imani
+
+		//galen
+
+		//chloe
+
+		//pedro
+
+		//mark j
+
+		//lucas
+
+		//tomas
+
+		// stephen
+
+		//other
+
+	];
+}
